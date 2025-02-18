3aa6fe5d6881a7dac10f17428957e551bbc2f47a
Mohit Panjikar <20210802090@dypiu.ac.in>
2025-02-18 09:47:12 +0530

    fs Module with Directories

--- FS Module with Directories.js
+++ FS Module with Directories.js
@@ -1,2 +1,16 @@
-const path = require('path');
+const fs = require('fs');
 
+//creating a Directory -
+// fs.mkdirSync('example_text_file');
+
+// Checking the content inside of a Directory
+const path = '/Users/mohitpanjikar/Desktop/Nodejs';
+const files = fs.readdirSync(path);
+console.log(files);
+
+//to check if Direcotries exist or not
+const check = fs.existsSync('/Users/mohitpanjikar/Desktop/Nodejs');
+console.log(check);
+
+//Deleting a Directory
+fs.rmdirSync('example_text_file');
