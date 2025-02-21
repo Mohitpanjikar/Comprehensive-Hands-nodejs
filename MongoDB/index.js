faad77ca98d4b15fb8a8b58871edbc3055841571
Mohit Panjikar <20210802090@dypiu.ac.in>
2025-02-21 20:37:31 +0530

    CRUD in mongoDB

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -19,8 +19,8 @@ const Course = mongoose.model('Course',courseSchema);
 async function createCourse(){
 
     const course = new Course({
-        name: 'GOlang',
-        creator: 'Sai',
+        name: 'python',
+        creator: 'rohan',
         ispublished: true,
         publishedDate: new Date(),
         rating:1
@@ -47,3 +47,40 @@ async function RatingCourse(){
 }   
 
 RatingCourse();
+
+//logical operators - or,and,not
+
+async function creatorCourse(){
+    const RatingCourse = await Course.find({$and: [{rating: {$gte: 4}}, {name: 'Python'}]}).select({name: 1, rating: 1});
+    console.log(RatingCourse);
+}
+
+creatorCourse();
+
+// Update Course -
+async function updateCourse(id) {
+    let course = await Course.findById(id);
+
+    if (!course) {
+        console.log("Course not found");
+        return;
+    }
+
+    course.name = "Ruby on Rails";
+    await course.save();  // Ensure to wait for the save operation to complete
+}
+
+updateCourse('67b8903b265caf58f1b5b48d');
+
+//Deleting course from database -
+async function DeleteCourse(id){
+    let course = await Course.findById(id);
+
+    if(!course){
+        console.log("Course not Found");
+        reurn;
+    }
+    await course.deleteOne();
+}
+
+DeleteCourse('67b8919a50dc5f46bf130d93');
